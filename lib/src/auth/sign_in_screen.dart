123af84c23c8c1ba27e94388a9379e33b8483ea8
Valter Noleto <valterlimjuniorr@gmail.com>
2022-06-18 18:36:22 -0300

    login screen

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,6 +1,4 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:my_sport/src/auth/sign_up_screen.dart';
 import 'package:my_sport/src/config/custom_colors.dart';
